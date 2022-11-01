e3387a0cc2591dc0c9a76d07b0d9c466876f5223
new-icarus <mcdcsr.tiago@gmail.com>
2022-11-01 09:28:32 -0300

    math methods
    round. floor. ceil. pow. sqrt. abs. max. min.

---
+++ 7_mathMethods/index.js
@@ -0,0 +1,29 @@
+let x = 3.5;
+let y = 5;
+let z = 9;
+let maximum;
+let minimum;
+
+x = Math.round(x);
+console.log(x);
+
+x = Math.ceil(x);
+console.log(x);
+
+x = Math.floor(x);
+console.log(x);
+
+x = Math.pow(x, 2);
+console.log(x);
+
+x = Math.sqrt(x);
+console.log(x);
+
+x = Math.abs(x);
+console.log(x);
+
+maximum = Math.max(x, y, z);
+console.log(maximum);
+
+minimum = Math.min(x, y, z);
+console.log(minimum);
