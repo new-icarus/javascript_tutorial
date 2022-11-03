1639eabc41a31114becb8905faccbe1f0570b663
new-icarus <mcdcsr.tiago@gmail.com>
2022-11-03 11:12:06 -0300

    Math.random
    rpg dice project. dices are generating the right number options.

---
+++ exercises/3_randomNumber/index.js
@@ -0,0 +1,9 @@
+let d20 = Math.floor(Math.random() * 20) + 1;
+let d12 = Math.floor(Math.random() * 12) + 1;
+let d10 = Math.floor(Math.random() * 10) + 1;
+let d8 = Math.floor(Math.random() * 8) + 1;
+let d6 = Math.floor(Math.random() * 6) + 1;
+let d4 = Math.floor(Math.random() * 4) + 1;
+
+
+console.log(`d20: ${d20}\nd12 : ${d12}\nd10: ${d10}\nd8: ${d8}\nd6: ${d6}\nd4: ${d4}`);
